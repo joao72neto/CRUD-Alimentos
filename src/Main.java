c1a4d2578f1deb1f500a7d8e611f295843d535dc
João Salvador Neto <joao72neto@gmail.com>
2024-09-22 15:26:04 -0300

    Termine a estrutura da interface

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.sql.SQLException;
-
+import java.util.InputMismatchException;
+import java.util.Scanner;
 import java.sql.Date;
 import entidades.*;
 
@@ -7,37 +8,157 @@ public class Main {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
-        Estilo.l(129);
-        System.out.printf("%55sCRUD de Alimentos\n", "");
-        Estilo.l(129);
+        //Variáveis
+        int TAM = 57;
 
-        //Guardando as datas
-        Date fabriacao = Date.valueOf("2035-08-12");
-        Date validade = Date.valueOf("2024-01-20");
+        //Título
+        Estilo.l(TAM);
+        System.out.println(Estilo.centralizar("CRUD de Alimentos", TAM));
+        Estilo.l(TAM);
 
-        //Criando uma tabela de alimentos
-        Alimentos prod = new Alimentos();
 
-        //Inserindo os dados na tabela
-        prod.setAlm_nome("Melão");
-        prod.setAlm_categoria("Fruta");
-        prod.setAlm_codigo_barras("123124124124");
-        prod.setAlm_data_validade(validade);
-        prod.setAlm_data_fabricacao(fabriacao);
-        prod.setAlm_pais_origem("Brasil");
-        prod.setAlm_peso_por_unidade(5);
-        prod.setAlm_quantidade_estoque(20);
-        prod.setAlm_marca("Do jão");
-        prod.setAlm_preco(1.2);
+        //Pedindo para o usuário inseriri os valores na tabela alimentos (apresentação)
+        Scanner sc = new Scanner(System.in);
+        Alimentos al = new Alimentos();
 
-        //Instanciando o CRUD
-        Crud c = new Crud();
 
-    
-        //Cadastrando a tabela de alimentos no banco de dados
-        c.visualizar(prod);
-        //c.atualizar(prod, "alm_nome", "Banana", 3);
-             
-       
+        System.out.print("Pressione qualquer tecla para adicionar dados a tabela...");
+        sc.nextLine();
+
+        //Limpando a tela do terminal
+        Estilo.limapTela();
+
+        //Pegando os dados do usuário
+        TAM = 40;
+        Estilo.l(TAM);
+        System.out.print("ID: ");
+        al.setAlm_id(sc.nextInt());
+        sc.nextLine();
+
+        System.out.print("NOME: ");
+        al.setAlm_nome(sc.nextLine());
+
+        System.out.print("CATEGORIA: ");
+        al.setAlm_categoria(sc.nextLine());
+
+        System.out.print("QUANTIDADE ESTOQUE:");
+        al.setAlm_quantidade_estoque(sc.nextDouble()); 
+
+        System.out.print("PREÇO: ");
+        al.setAlm_preco(sc.nextDouble());
+        sc.nextLine();
+
+        System.out.print("DATA DE VALIDADE: ");
+        String validade = sc.nextLine();
+        Date validade_formatada = Date.valueOf(validade);
+        al.setAlm_data_validade(validade_formatada);
+
+
+        System.out.print("DATA DE FABRICAÇÃO: ");
+        String fabricacao = sc.nextLine();
+        Date fabricacao_formatada = Date.valueOf(fabricacao);
+        al.setAlm_data_fabricacao(fabricacao_formatada);
+
+        System.out.print("PESO POR UNIDADE: ");
+        al.setAlm_peso_por_unidade(sc.nextDouble());
+        sc.nextLine();
+
+        System.out.print("MARCA: ");
+        al.setAlm_marca(sc.nextLine());
+
+        System.out.print("PAÍS ORIGEM: ");
+        al.setAlm_pais_origem(sc.nextLine());
+
+        System.out.print("CÓDIGO DE BARRAS: ");
+        al.setAlm_codigo_barras(sc.nextLine());
+        
+        //Variáveis que permite sair do loop while
+        boolean rodar = true; 
+
+        while (rodar){
+
+            //Título
+            Estilo.limapTela();
+            Estilo.l(TAM);
+            System.out.println(Estilo.centralizar("CRUD de Alimentos", TAM));
+            Estilo.l(TAM);
+
+            //Menu
+            System.out.println("1 - Cadastrar");
+            System.out.println("2 - Visualizar");
+            System.out.println("3 - Atualizar");
+            System.out.println("4 - Deletar");
+            System.out.println("5 - Sair");
+            Estilo.l(TAM);
+            System.out.print("Sua escolha: ");
+
+            //Variáveis para pegar a escolha do usuário  
+            int escolha = 0;
+
+            //Pegando a escolha do usuário
+            try {
+                escolha = sc.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Tipo Iválido " + e.toString());
+            } 
+
+            //Limpando o buffer 
+            sc.nextLine();
+
+            //Isntanciando o crud e a tabela
+            Crud c = new Crud();
+            
+            //Verificando a escolha do usuário
+            switch (escolha) {
+                case 1:
+                    Estilo.limapTela();
+                    c.cadastrar(al);
+
+                    //Feedback par ao usuário
+                    Estilo.l(TAM);
+                    System.out.println(Estilo.centralizar("Alimento cadastrado com sucesso!", TAM));
+                    Estilo.l(TAM);
+                    System.out.print("Pressione qualque tecla para voltar...");
+                    sc.nextLine();
+                    break;
+
+                case 2:
+                    Estilo.limapTela();
+                    c.visualizar(al);
+                    Estilo.l(TAM);
+                    System.out.print("Pressione qualque tecla para voltar...");
+                    sc.nextLine();
+                    break;
+            
+                case 3:
+                    System.out.println("Nada aqui");
+                    System.out.print("Pressione qualque tecla para voltar...");
+                    sc.nextLine();
+                    break;
+
+                case 4:
+                    System.out.println("Nada aqui");
+                    System.out.print("Pressione qualque tecla para voltar...");
+                    sc.nextLine();
+                    break;
+
+                case 5:
+                    rodar = false;
+                    break;
+
+                default:
+                    Estilo.l(TAM);
+                    System.out.println(Estilo.centralizar("Opção inválida", TAM));
+                    Estilo.l(TAM);
+                    System.out.print("Pressione qualque tecla para voltar...");
+                    sc.nextLine();
+                    break;
+            }
+        }  
+
+        Estilo.limapTela();
+        Estilo.l(TAM);
+        System.out.println(Estilo.centralizar("Fim do CRUD", TAM));
+        Estilo.l(TAM);
     }
 }
--- src/entidades/Estilo.java
+++ src/entidades/Estilo.java
@@ -4,16 +4,33 @@ public class Estilo {
     
     //Funções que printam uma linha na tela
     public static void l(){
-        for (int i=0; i < 40; i++){
-            System.out.print("-");
-        }
-        System.out.println();
+        System.out.println("-".repeat(40));
     }
 
     public static void l(int TAM){
-        for (int i=0; i < TAM; i++){
-            System.out.print("-");
-        }
-        System.out.println();
+        System.out.println("-".repeat(TAM));
     }
-}
+
+    //Função que centraliza um string
+    public static String centralizar(String msg, int TAM){
+
+        int lateral = (TAM - msg.length()) / 2;
+        return " ".repeat(lateral) + msg;
+
+    }
+
+    //Função que limpa a tela
+    public static void limapTela(){
+
+        try{
+            if (System.getProperty("os.name").contains("Windows")){
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            }else{  
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        }catch(Exception e){
+            System.out.println("Erro: " + e.toString());
+        }
+        
+    }
+}   
